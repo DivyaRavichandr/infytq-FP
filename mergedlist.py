82382fa5c8ed167748d97c8d7aca75ff3519d07e
DivyaRavichandr <47322040+DivyaRavichandr@users.noreply.github.com>
2019-05-09 14:58:36 +0530

    Create mergedlist.py

---
+++ mergedlist.py
@@ -0,0 +1,16 @@
+def merge_lists(list1,list2):
+    merged_list=list1+list2
+
+    return merged_list
+def sort_list(merged_list):
+    sorted_merged_list=sorted(merged_list)
+    
+
+    return sorted_merged_list
+
+#Provide different values for list1 and list2 and test your program
+merged_list=merge_lists(list1=[1,2,3,4,1] ,list2=[2,3,4,5,4,6])
+print(merged_list)
+
+sorted_merged_list=sort_list(merged_list)
+print(sorted_merged_list)
