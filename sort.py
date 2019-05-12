49dc9aeea4997ed68e7093b6c65dc234136ea920
DivyaRavichandr <47322040+DivyaRavichandr@users.noreply.github.com>
2019-05-12 14:47:02 +0530

    Create sort.py

---
+++ sort.py
@@ -0,0 +1,17 @@
+vowels = ['e', 'a', 'u', 'o', 'i']
+
+# sort the vowels
+vowels.sort(reverse=True)
+
+# print vowels
+print('Sorted list (in Descending):', vowels)
+
+**********************************************************
+
+vowels = ['e', 'a', 'u', 'o', 'i']
+
+# sort the vowels
+vowels.sort()
+
+# print vowels
+print('Sorted list:', vowels)
