6c4a3369010b064d2a909ee03d71ab8079a25ae9
DivyaRavichandr <47322040+DivyaRavichandr@users.noreply.github.com>
2019-05-13 09:40:09 +0530

    Update patient.py

--- patient.py
+++ patient.py
@@ -1,18 +1,26 @@
 def max_visited_speciality(patient_medical_speciality_list,medical_speciality):
-    count=0
-    for i in patient_medical_speciality_list:
-        if i==x:
-            count=count+1 
-            i=i+1
-patient_medical_speciality_list=[ 102,'E',302,'P',305,'E',401,'O',656,'E',987,'O']
-medical_speciality={"P":"Pediatrics","O":"Orthopedics","E":"ENT"}
+    medical_speciality_list=[]
+    for i in range(0,len(patient_medical_speciality_list),2):
+            medical_speciality_list.append(patient_medical_speciality_list[i+1])
+    
+    count_list=[0]*3
+    for i in medical_speciality_list:
+        if i=="P":
+            count_list[0]+=1
+        elif i=="O":
+            count_list[1]+=1
+        elif i=="E":
+            count_list[2]+=1
+    medical_list=["P","O","E"]
+    maxx=max(count_list)
+    for i in range(len(count_list)):
+        if count_list[i]==maxx:
+            temp=i
+            result=medical_list[temp]
+    return(medical_speciality[result])        
 
-x=max(patient_medical_speciality_list,key=patient_medical_speciality_list.count)
-if x=="P":
-  print("Pediatrics")
-elif x=="O":
-  print("Orthopedics")
-elif x=="E":                
-  print("ENT")
-else:    
-    print("")
+patient_medical_speciality_list=[301,'P',302, 'P' ,305, 'P' ,401, 'E' ,656, 'E']
+medical_speciality={"P":"Pediatrics","O":"Orthopedics","E":"ENT"}
+speciality=max_visited_speciality(patient_medical_speciality_list,medical_speciality)
+print(speciality)
+© 2019 GitHub, Inc.
