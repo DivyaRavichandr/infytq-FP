09775e644a381e8179524f6fedf6561e37f83b99
DivyaRavichandr <47322040+DivyaRavichandr@users.noreply.github.com>
2019-05-13 08:28:02 +0530

    Update possiblesum.py

--- possiblesum.py
+++ possiblesum.py
@@ -1,12 +1,18 @@
 def find_pairs_of_numbers(num_list,n):
     count=0
-    for i in range(0,s):
-        for j in range(i+1,s):
-            if(num_list[i]+num_list[j]==n):
+    for i in range(len(num_list)):
+        temp=i
+        for j in range(len(num_list)):
+            if (num_list[i]+num_list[j]==n) and (i!=j):
                 count+=1
-    return count        
+                
+                num_list[i]=0
+                num_list[j]=0
+               
+                
+    return(count)
+    
 
 num_list=[1, 2, 4, 5, 6]
-s=len(num_list)
 n=6
 print(find_pairs_of_numbers(num_list,n))
